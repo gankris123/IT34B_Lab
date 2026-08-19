7f6f94bb3b1b886b02a2a9f246e97439f1fabac0
gankris123 <ganiemodesto300@gmail.com>
2026-08-19 15:26:22 +0800

    added PDO logs

--- includes/activity-logger.php
+++ includes/activity-logger.php
@@ -1,14 +1,35 @@
 <?php
     function logActivity($pdo,$user_id,$email,$action,$status='success'){
-        try{
+        try {
             // Get Client IP Address
             $ip = $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'] ?? 'UNKNOWN';
 
+           //String to Array
+           if(strpos($ip, ',') !== false){
+                $ip = trim(explode(',', $ip)[0]);
 
+           }
 
-        }catch(PDOException $e){
-            error_log("Activity Log Error:" .$e->getMessage());
+           // Get user agent (browser) 
+           $user_agent = substr($SERVER['HTTP_USER_AGENT'] ?? 'UNKNOWN', 0, 255);
+
+           // Application Query #1
+           $stmt = $pdo -> prepare("
+             INSERT INTO activity_logs(
+                 user_id,
+                 user_email,
+                 activity_log_action,
+                 activity_log_status,
+                 activity_log_ip_address,
+                 activity_log_user_agent
+             ) VALUES (?,?,?,?,?,?)
+        ");
+            
+
+        } catch (PDOException $e){
+         
+            error_log("Activity Log Error: " . $e->getMessage());
             return false;
             }
     }
- 
+ ?>
